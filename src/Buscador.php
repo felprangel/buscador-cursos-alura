1614fc0cea9a4f0b74b938558cc3d4ed97ef0741
felprangel <feliperangel897@gmail.com>
2024-11-26 23:08:40 -0300

    adiciona classe de buscador

---
+++ src/Buscador.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Felprangel\BuscadorCursosAlura;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    public function __construct(public ClientInterface $httpClient, public Crawler $crawler)
+    {
+    }
+
+    public function buscar(string $url): array
+    {
+        $resposta = $this->httpClient->request('GET', $url);
+
+        $html = $resposta->getBody();
+
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
